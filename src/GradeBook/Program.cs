6b09c28a9ade8308172a7c9110967a3817ea367c
ZachPfeifer <mr.zman1@yahoo.com>
2020-04-14 14:08:25 -0600

    Reviewing Data-Types & List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,6 +8,30 @@ namespace GradeBook
         //Method Main
         static void Main(string[] args)
         {
+//SECTION Grades
+             //NOTE var represents Double(auto detect the data-type)
+            //    var x = 34.1;
+            //    var y = 10.3;
+            //    var result = x + y;
+            //    Console.WriteLine(result);
+
+            //NOTE initilizing array of double data-type numbers
+            var numbers = new [] {12.7,10.3, 6.11, 4.1};
+            //NOTE List are used to dynamically add and remove objects from List/Array(only data-type doubles can be added to Generic List here)
+            var grades = new List<double>(){12.7,10.3, 6.11, 4.1};
+            grades.Add(56.1);
+        
+            var result= 0.0;
+            foreach(var number in grades)
+            {
+               result += number;
+            }
+            result /=grades.Count;
+            //NOTE N2 is similar to .toFixed(2)
+            Console.WriteLine($"The average grade is {result:N2}");
+
+
+//SECTION Greeting
            if( args.Length > 0)
            {
             Console.WriteLine($"Hello, {args[0]}!");
