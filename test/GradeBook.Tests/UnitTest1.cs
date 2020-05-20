be4decec6aa2d06391fdc262ed431aae9e3db096
ZachPfeifer <mr.zman1@yahoo.com>
2020-05-20 13:18:45 -0600

    Added Xunit test

---
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]//NOTE Attribute ( a little piece of data that is attached to this method)
+        public void Test1()
+        {
+
+        }
+    }
+}
